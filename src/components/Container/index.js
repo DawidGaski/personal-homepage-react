6581546eb098adc1be1e1c62138e00ba1f76e838
Dawid Galeski <dawidgaski@gmail.com>
2024-05-24 16:18:25 +0200

    created Container component

---
+++ src/components/Container/index.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1216px;
+  margin: auto;
+  display: grid;
+  grid-template-columns: auto;
+  grid-gap: 72px;
+`;
