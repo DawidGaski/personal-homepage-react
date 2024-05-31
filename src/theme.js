379c830b701e917552c6dd10640050e4e05b62c9
Dawid Galeski <dawidgaski@gmail.com>
2024-05-31 20:03:56 +0200

    new colors and breakpoints addded

--- src/theme.js
+++ src/theme.js
@@ -1,9 +1,16 @@
 export const theme = {
   color: {
     white: "#FFFFFF",
+    liteIron: "#D1D5DA4D",
     iron: "#D1D5DA",
     slateGrey: "#6E7E91",
     scienceBlue: "#0366D6",
     mineShaft: "#252525",
+    liteViolet: "#090A3308",
+    veryLiteViolet: "#090A3305",
+  },
+  breakpoint: {
+    mobileMax: 767,
+    tabletMax: 1250,
   },
 };
