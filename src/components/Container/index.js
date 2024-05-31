bdec0bcbff5692adee86ee02ab5b2ab91b8e60ef
Dawid Galeski <dawidgaski@gmail.com>
2024-05-31 20:03:04 +0200

    padding-top added

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   max-width: 1216px;
   margin: auto;
+  padding-top: 120px;
   display: grid;
   grid-template-columns: auto;
   grid-gap: 72px;
