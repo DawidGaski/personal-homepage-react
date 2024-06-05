232f3f33fb15359e0ecc69a5c21fab1cbbcc976e
Dawid Galeski <dawidgaski@gmail.com>
2024-06-05 12:32:24 +0200

    created TextIcon Component

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -8,3 +8,13 @@ export const Icon = styled.img`
     height: 16px;
   }
 `;
+
+export const TextIcon = styled.img`
+  vertical-align: middle;
+  width: 20px;
+  height: 20px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    height: 16px;
+  }
+`;
