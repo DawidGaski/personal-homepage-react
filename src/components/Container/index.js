29559d3d4f9befb7e940b7e273c54cdd9d314074
Dawid Galeski <dawidgaski@gmail.com>
2024-06-05 12:33:40 +0200

    added padding-bottom in Container

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -4,6 +4,7 @@ export const Container = styled.div`
   max-width: 1216px;
   margin: auto;
   padding-top: 120px;
+  padding-bottom: 120px;
   display: grid;
   grid-template-columns: auto;
   grid-gap: 72px;
