38bc34778732ce13d3cb1a38d44200934b9d1e06
Dawid Galeski <dawidgaski@gmail.com>
2024-06-06 15:18:59 +0200

    created MySkills component

--- src/App.js
+++ src/App.js
@@ -8,17 +8,17 @@ import {
   LeadParagraph,
   Title,
 } from "./components/Header";
-import { Headline, Item, List, Section } from "./components/Section";
+import { Headline, Item, List, Section } from "./components/Section/styled";
 import { Icon, TextIcon } from "./components/Icon";
 import { Portfolio } from "./features/PersonalHomepage/Portfolio";
 import { EndSection } from "./components/End";
 import Poster from "./images/Poster.jpg";
-import Tools from "./images/Tools.png";
 import Rocket from "./images/Rocket.png";
 import Message from "./images/Message.png";
 import Human from "./images/Human.png";
 import Laptop from "./images/Laptop.png";
 import { email } from "./features/PersonalHomepage/email";
+import { MySkills } from "./features/PersonalHomepage/MySkills";
 
 function App() {
   return (
@@ -40,40 +40,7 @@ function App() {
           </Button>
         </Content>
       </Hero>
-      <Section>
-        <Headline>
-          My skillset includes <Icon src={Tools} alt="" />
-        </Headline>
-        <List>
-          <Item>HTML</Item>
-          <Item>Teamwork</Item>
-          <Item>Markdown</Item>
-          <Item>CSS Bem Convention</Item>
-          <Item>CSS Flexbox</Item>
-          <Item>Css Grid</Item>
-          <Item>Immutability</Item>
-          <Item>React</Item>
-          <Item>Redux</Item>
-          <Item>HTML</Item>
-          <Item>Teamwork</Item>
-          <Item>Markdown</Item>
-          <Item>CSS Bem Convention</Item>
-          <Item>CSS Flexbox</Item>
-          <Item>Css Grid</Item>
-          <Item>Immutability</Item>
-          <Item>React</Item>
-          <Item>Redux</Item>
-          <Item>HTML</Item>
-          <Item>Teamwork</Item>
-          <Item>Markdown</Item>
-          <Item>CSS Bem Convention</Item>
-          <Item>CSS Flexbox</Item>
-          <Item>Css Grid</Item>
-          <Item>Immutability</Item>
-          <Item>React</Item>
-          <Item>Redux</Item>
-        </List>
-      </Section>
+      <MySkills />
       <Section>
         <Headline>
           What I want to learn next <Icon src={Rocket} alt="" />{" "}
---
+++ src/features/PersonalHomepage/MySkills/index.js
@@ -0,0 +1,45 @@
+import {
+  Section,
+  Headline,
+  List,
+  Item,
+} from "../../../components/Section/styled";
+import { Icon } from "../../../components/Icon";
+import Tools from "../../../images/Tools.png";
+
+export const MySkills = () => (
+  <Section>
+    <Headline>
+      My skillset includes <Icon src={Tools} alt="" />
+    </Headline>
+    <List>
+      <Item>HTML</Item>
+      <Item>Teamwork</Item>
+      <Item>Markdown</Item>
+      <Item>CSS Bem Convention</Item>
+      <Item>CSS Flexbox</Item>
+      <Item>Css Grid</Item>
+      <Item>Immutability</Item>
+      <Item>React</Item>
+      <Item>Redux</Item>
+      <Item>HTML</Item>
+      <Item>Teamwork</Item>
+      <Item>Markdown</Item>
+      <Item>CSS Bem Convention</Item>
+      <Item>CSS Flexbox</Item>
+      <Item>Css Grid</Item>
+      <Item>Immutability</Item>
+      <Item>React</Item>
+      <Item>Redux</Item>
+      <Item>HTML</Item>
+      <Item>Teamwork</Item>
+      <Item>Markdown</Item>
+      <Item>CSS Bem Convention</Item>
+      <Item>CSS Flexbox</Item>
+      <Item>Css Grid</Item>
+      <Item>Immutability</Item>
+      <Item>React</Item>
+      <Item>Redux</Item>
+    </List>
+  </Section>
+);
