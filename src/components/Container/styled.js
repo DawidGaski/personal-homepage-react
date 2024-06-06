8dfc6580b9c316123e553c664ff955009df58ce0
Dawid Galeski <dawidgaski@gmail.com>
2024-06-06 17:55:28 +0200

    renamed file in Container

---
+++ src/components/Container/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  max-width: 1216px;
+  margin: auto;
+  padding-top: 120px;
+  padding-bottom: 120px;
+  display: grid;
+  grid-template-columns: auto;
+  grid-gap: 72px;
+`;
