82a67dc900d6c9e2b454b26d3bc0c703a567e973
Dawid Galeski <dawidgaski@gmail.com>
2024-06-06 17:54:55 +0200

    new colors added

--- src/theme.js
+++ src/theme.js
@@ -6,6 +6,8 @@ export const theme = {
     iron: "#D1D5DA",
     slateGrey: "#6E7E91",
     scienceBlue: "#0366D6",
+    liteScienceBlue: "#0366d633",
+    dodgerBlue: "2188FF",
     mineShaft: "#252525",
     liteViolet: "#090A3308",
     veryLiteViolet: "#090A3305",
