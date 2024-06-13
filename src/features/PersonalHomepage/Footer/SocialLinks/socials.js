d6f930bcdcd4da100b9394d4184957dfa8699c12
Dawid Galeski <dawidgaski@gmail.com>
2024-06-13 15:31:08 +0200

    added link to Linkedin in footer

--- src/features/PersonalHomepage/Footer/SocialLinks/socials.js
+++ src/features/PersonalHomepage/Footer/SocialLinks/socials.js
@@ -1,6 +1,7 @@
 import { styledIcon } from "./styled";
 import { ReactComponent as GithubIcon } from "./socialIcons/GithubIcon.svg";
 import { ReactComponent as FaceBookIcon } from "./socialIcons/FacebookIcon.svg";
+import { ReactComponent as LinkedinIcon } from "./socialIcons/LinkedIN.svg";
 
 export const socials = [
   {
@@ -12,5 +13,11 @@ export const socials = [
     name: "Facebook",
     url: "https://www.facebook.com/dawid.galeski",
     Icon: styledIcon(FaceBookIcon),
+  },
+
+  {
+    name: "Linkedin",
+    url: "https://www.linkedin.com/in/dawid-ga%C5%82%C4%99ski-74585a313/",
+    Icon: styledIcon(LinkedinIcon),
   },
 ];
